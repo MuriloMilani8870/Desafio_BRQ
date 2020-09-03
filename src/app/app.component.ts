6288b5101966c15188b228babe6ddec2b6011c0f
Murilo Milani <murilomilani8870@gmail.com>
2020-09-03 18:03:59 -0300

    Commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import * as dayjs from 'dayjs'
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CRUD';
+  
+  success = function(){
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    })
+  };
+}
